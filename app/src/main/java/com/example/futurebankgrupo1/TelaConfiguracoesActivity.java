29bb88387b8886bc885eecdf2639f42b2659a844
Rafael Silverio <rafael.silverio@foursys.com.br>
2022-06-03 14:45:45 -0300

    Tela configuração app, tela dados da conta, binding cobrar pix

---
+++ app/src/main/java/com/example/futurebankgrupo1/TelaConfiguracoesActivity.java
@@ -0,0 +1,14 @@
+package com.example.futurebankgrupo1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaConfiguracoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_configuracoes);
+    }
+
+}
