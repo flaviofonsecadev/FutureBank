2ac06734925829d13590a820fd5376ab2dc0abad
Rafael Silverio <rafael.silverio@foursys.com.br>
2022-06-09 21:16:03 -0300

    layouts e bindings

--- app/src/main/java/com/example/futurebankgrupo1/MyViewModel.java
+++ app/src/main/java/com/example/futurebankgrupo1/MyViewModel.java
@@ -1,18 +1,21 @@
 package com.example.futurebankgrupo1;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
 
-    private MutableLiveData<Double> saldoContaCorrente = new MutableLiveData<>();
 
-    //public LiveData<Float> valorFA = valorFaturaAtual;
-    private MutableLiveData<Integer> saldoContaPoupanca = new MutableLiveData<>();
 
-    private MutableLiveData<Integer> valorFaturaAtual = new MutableLiveData<>();
+    public MutableLiveData<Double> saldoContaCorrente = new MutableLiveData<>();
+    public LiveData<Double> saldoCC = saldoContaCorrente;
 
-    private MutableLiveData<Integer> limiteDisponivel = new MutableLiveData<>();
+    public MutableLiveData<Integer> saldoContaPoupanca = new MutableLiveData<>();
+
+    public MutableLiveData<Integer> valorFaturaAtual = new MutableLiveData<>();
+
+    public MutableLiveData<Integer> limiteDisponivel = new MutableLiveData<>();
     //public LiveData<Float> limiteD = limiteDisponivel;
 
 
@@ -20,7 +23,7 @@ public class MyViewModel extends ViewModel {
     public MyViewModel() {
         limiteDisponivel.setValue(1000);
         valorFaturaAtual.setValue(0);
-        saldoContaCorrente.setValue(5000d);
+        saldoContaCorrente.setValue(5000.00d);
         saldoContaPoupanca.setValue(2500);
     }
 
@@ -42,6 +45,16 @@ public class MyViewModel extends ViewModel {
     public float exibirSaldoContaPoupanca() {
         float saldoCP = saldoContaPoupanca.getValue();
         return saldoCP;
+    }
+
+    public MutableLiveData<Double> comprarCartaoCredito() {
+        double saldo = saldoContaCorrente.getValue();
+        double compra = 20;
+
+        saldoContaCorrente.setValue(saldo - compra);
+
+        return saldoContaCorrente;
+
     }
 
     /*public void comprarCredito() {
