530ca8bc53b2fe8369e60177cb45b3025b7d2961
Flavio Fonseca <flavio.fonseca@foursys.com.br>
2022-06-23 09:05:42 -0300

    tentativa puxar dados usuario da api mock

--- app/src/main/java/com/example/futurebankgrupo1/User.java
+++ app/src/main/java/com/example/futurebankgrupo1/User.java
@@ -1,9 +1,8 @@
 package com.example.futurebankgrupo1;
 
-// Classes User e Adress
 
 public class User {
-    private Integer id;
+    private int id;
     private String name,
             cpf,
             birthDate,
@@ -12,25 +11,26 @@ public class User {
             password;
     private Adress adress;
 
-    public User(){
-    }
 
-    public User(Integer id, String name, String cpf, String birthDate, String email, String phone, String password, Adress adress) {
-        this.id = id;
-        this.name = name;
-        this.cpf = cpf;
-        this.birthDate = birthDate;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.adress = adress;
-    }
+//    public User(){
+//    }
 
-    public Integer getId() {
+//    public User(int id, String name, String cpf, String birthDate, String email, String phone, String password, Adress adress) {
+//        this.id = id;
+//        this.name = name;
+//        this.cpf = cpf;
+//        this.birthDate = birthDate;
+//        this.email = email;
+//        this.phone = phone;
+//        this.password = password;
+//        this.adress = adress;
+//    }
+
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -91,93 +91,4 @@ public class User {
     }
 }
 
-// Classe Adress
 
-class Adress{
-    private Number	id;
-    private String zipcode;
-    private Number number;
-    private String publicPlace,
-            district,
-            city,
-            state,
-            country;
-
-    public Adress(){
-    }
-
-    public Adress(Number id, String zipcode, Number number, String publicPlace, String district, String city, String state, String country) {
-        this.id = id;
-        this.zipcode = zipcode;
-        this.number = number;
-        this.publicPlace = publicPlace;
-        this.district = district;
-        this.city = city;
-        this.state = state;
-        this.country = country;
-    }
-
-    public Number getId() {
-        return id;
-    }
-
-    public void setId(Number id) {
-        this.id = id;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public Number getNumber() {
-        return number;
-    }
-
-    public void setNumber(Number number) {
-        this.number = number;
-    }
-
-    public String getPublicPlace() {
-        return publicPlace;
-    }
-
-    public void setPublicPlace(String publicPlace) {
-        this.publicPlace = publicPlace;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-}
