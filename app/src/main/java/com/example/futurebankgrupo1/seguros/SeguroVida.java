c76f6e65cb3dc7d4aba131faf86060c59aaea1a8
Flavio Fonseca <flavio.fonseca@foursys.com.br>
2022-06-23 16:19:07 -0300

    Descontar do saldo a contratação de Seguro de vida e Seguro do cartão

--- app/src/main/java/com/example/futurebankgrupo1/seguros/SeguroVida.java
+++ app/src/main/java/com/example/futurebankgrupo1/seguros/SeguroVida.java
@@ -1,5 +1,6 @@
 package com.example.futurebankgrupo1.seguros;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,20 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.futurebankgrupo1.HomeActivity;
+import com.example.futurebankgrupo1.contas.ContaCorrenteActivity;
 import com.example.futurebankgrupo1.databinding.ActivityHomeBinding;
 import com.example.futurebankgrupo1.databinding.ActivitySeguroVidaBinding;
+import com.example.futurebankgrupo1.fatura.pagarfatura.ComprovanteFatura;
+import com.example.futurebankgrupo1.fatura.pagarfatura.PagarFaturaConfirmarValor;
+import com.example.futurebankgrupo1.usuario.UserFirebase;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SeguroVida extends AppCompatActivity {
 
@@ -42,13 +55,63 @@ public class SeguroVida extends AppCompatActivity {
             binding.cbPessoas.setChecked(false);
         });
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        String userID = user.getUid();
+        binding.btnContratarSeguroVida.setOnClickListener(view1 -> {
 
-        binding.btnContratarSeguroVida.setOnClickListener(v -> {
-            if(binding.cbIndividual.isChecked() || binding.cbPessoas.isChecked() || binding.cbFamilia.isChecked()) {
-                Toast.makeText(this, "Plano contratado!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Nenhum plano selecionado!", Toast.LENGTH_SHORT).show();
-            }
+            reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    UserFirebase userProfile = snapshot.getValue(UserFirebase.class);
+                    if (userProfile != null){
+                        float saldo = userProfile.getSaldo();
+
+                        if (binding.cbIndividual.isChecked()){
+                            if (saldo >= 37.90){
+                                reference.child(userID).child("saldo").setValue(saldo - 37.90);
+                                Toast.makeText(SeguroVida.this, "Plano individual contratado!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                            }else {
+                                Toast.makeText(SeguroVida.this, "Saldo insuficiente!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        else if (binding.cbPessoas.isChecked()){
+                            if (saldo >= 43.90){
+                                reference.child(userID).child("saldo").setValue(saldo - 43.90);
+                                Toast.makeText(SeguroVida.this, "Plano para duas pessoas contratado!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(SeguroVida.this, "Saldo insuficiente!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        else if (binding.cbFamilia.isChecked()){
+                            if (saldo >= 59.90){
+                                reference.child(userID).child("saldo").setValue(saldo - 59.90);
+                                Toast.makeText(SeguroVida.this, "Plano família contratado!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(SeguroVida.this, "Saldo insuficiente!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else if(!binding.cbIndividual.isChecked() || !binding.cbPessoas.isChecked() || !binding.cbFamilia.isChecked()) {
+                            Toast.makeText(SeguroVida.this, "Nenhum plano selecionado!", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(SeguroVida.this, "Ocorreu algum erro, tente novamente!", Toast.LENGTH_LONG).show();
+                }
+            });
+
         });
+
     }
 }
